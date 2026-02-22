992862b43f79eafbd88c95fa6d23cd3eafdc91b9
Dhiraj Shelke <130848181+Dhiraj1103@users.noreply.github.com>
2026-02-22 21:20:13 +0530

    Update RoomReservation.java

--- HotelManagement/src/com/hotel/model/RoomReservation.java
+++ HotelManagement/src/com/hotel/model/RoomReservation.java
@@ -7,25 +7,32 @@ import com.hotel.model.reservations.Reservation;
 public class RoomReservation extends Reservation {
 
     private String roomNumber;
-    private int    numberOfNights;
+    private int numberOfNights;
     private double pricePerNight;
 
-    public RoomReservation(String reservationId, String guestId,
-                           LocalDate reservationDate, String roomNumber,
-                           int numberOfNights, double pricePerNight) {
+    public RoomReservation(String reservationId, String guestId, LocalDate reservationDate, String roomNumber,int numberOfNights, double pricePerNight) {
         super(reservationId, guestId, reservationDate);
-        this.roomNumber     = roomNumber;
+        this.roomNumber  = roomNumber;
         this.numberOfNights = numberOfNights;
         this.pricePerNight  = pricePerNight;
     }
 
-    public String getRoomNumber()    { return roomNumber; }
-    public int    getNumberOfNights(){ return numberOfNights; }
+    public String getRoomNumber()    { 
+        return roomNumber; 
+    }
+    public int getNumberOfNights(){ 
+        return numberOfNights;
+    }
 
     @Override
-    public String getReservationType() { return "RoomReservation"; }
+    public String getReservationType() { 
+        return "RoomReservation"; 
+    }
 
     @Override
-    public double getTotalCost() { return pricePerNight * numberOfNights; }
+    public double getTotalCost() { 
+        return pricePerNight * numberOfNights; 
+    }
 }
+
 
