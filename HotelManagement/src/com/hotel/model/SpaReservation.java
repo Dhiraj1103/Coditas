9c3c303c21dc5a3d4d37ab67b6227f45da157e20
Dhiraj Shelke <130848181+Dhiraj1103@users.noreply.github.com>
2026-02-22 21:20:54 +0530

    Update SpaReservation.java

--- HotelManagement/src/com/hotel/model/SpaReservation.java
+++ HotelManagement/src/com/hotel/model/SpaReservation.java
@@ -11,20 +11,27 @@ public class SpaReservation extends Reservation {
 
     private static final double RATE_PER_MINUTE = 50;
 
-    public SpaReservation(String reservationId, String guestId,
-                          LocalDate reservationDate, String treatmentType,
-                          int durationMinutes) {
+    public SpaReservation(String reservationId, String guestId, LocalDate reservationDate, String treatmentType, int durationMinutes) {
         super(reservationId, guestId, reservationDate);
         this.treatmentType   = treatmentType;
         this.durationMinutes = durationMinutes;
     }
 
-    public String getTreatmentType()  { return treatmentType; }
-    public int    getDurationMinutes(){ return durationMinutes; }
+    public String getTreatmentType()  { 
+        return treatmentType;
+    }
+    public int getDurationMinutes(){ 
+        return durationMinutes; 
+    }
 
     @Override
-    public String getReservationType() { return "SpaReservation"; }
+    public String getReservationType() {
+        return "SpaReservation"; 
+    }
 
     @Override
-    public double getTotalCost() { return RATE_PER_MINUTE * durationMinutes; }
+    public double getTotalCost() { 
+        return RATE_PER_MINUTE * durationMinutes; 
+    }
+
 }
