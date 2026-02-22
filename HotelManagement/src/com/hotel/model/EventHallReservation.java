9244c9d5c25d34de9e4cfb93db0ec3435843c9c7
Dhiraj Shelke <130848181+Dhiraj1103@users.noreply.github.com>
2026-02-22 21:12:47 +0530

    Update EventHallReservation.java

--- HotelManagement/src/com/hotel/model/EventHallReservation.java
+++ HotelManagement/src/com/hotel/model/EventHallReservation.java
@@ -7,24 +7,31 @@ import com.hotel.model.reservations.Reservation;
 public class EventHallReservation extends Reservation {
 
     private String hallName;
-    private int    numberOfHours;
+    private int numberOfHours;
 
     private static final double HOURLY_RATE = 700.00;
 
-    public EventHallReservation(String reservationId, String guestId,
-                                LocalDate reservationDate, String hallName,
-                                int numberOfHours) {
+    public EventHallReservation(String reservationId, String guestId, LocalDate reservationDate, String hallName, int numberOfHours) {
         super(reservationId, guestId, reservationDate);
-        this.hallName      = hallName;
+        this.hallName = hallName;
         this.numberOfHours = numberOfHours;
     }
 
-    public String getHallName()     { return hallName; }
-    public int    getNumberOfHours(){ return numberOfHours; }
+    public String getHallName() { 
+        return hallName; 
+    }
+    public int getNumberOfHours(){ 
+        return numberOfHours; 
+    }
 
     @Override
-    public String getReservationType() { return "EventHallReservation"; }
+    public String getReservationType() {
+        return "EventHallReservation"; 
+    }
 
     @Override
-    public double getTotalCost() { return HOURLY_RATE * numberOfHours; }
+    public double getTotalCost() {
+        return HOURLY_RATE * numberOfHours; 
+    }
+
 }
